c1d34a6645b30b5ab8c3b920b9d7c312cca3586f
Totemrecall1 <philm2004@gmail.com>
2024-11-26 13:19:11 +0000

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,205 @@
+local RingMenu_AddonName, RingMenu = ...
+local Masque, MasqueVersion = LibStub("Masque", true)
+
+local RingMenu_globalConfigDefault = {
+    numRings = 1,
+    allowMultipleOpenRings = false,
+}
+
+local RingMenu_ringConfigDefault = {
+    name = nil,
+    keyBind = nil,
+    closeOnClick = true,
+    radius = 100,
+    angle = 0,
+    firstSlot = 13,
+    numSlots = 12,
+    backdropScale = 1.5,
+    backdropColor = {
+        r = 0.0,
+        g = 0.0,
+        b = 0.0,
+        a = 0.5,
+    },
+}
+
+local RingMenu_globalStateDefault = {
+}
+
+local RingMenu_ringStateDefault = {
+}
+
+-- Global variables for settings and ring state.
+-- These will be updated with actual values in ADDON_LOADED.
+RingMenu_globalConfig = {}
+RingMenu_ringConfig = {}
+RingMenu.globalState = {}
+RingMenu.ringState = {}
+
+function RingMenu_AddRing()
+    RingMenu_globalConfig.numRings = RingMenu_globalConfig.numRings + 1
+    local ringID = RingMenu_globalConfig.numRings
+    RingMenu_ringConfig[ringID] = RingMenu.deep_copy(RingMenu_ringConfigDefault)
+    RingMenu.ringState = RingMenu.deep_copy(RingMenu_ringStateDefault)
+    RingMenu_UpdateAllRings()
+    return ringID
+end
+
+function RingMenu_RemoveRing(ringID)
+    table.remove(RingMenu_ringConfig, ringID)
+    table.remove(RingMenu.ringState, ringID)
+    RingMenu_globalConfig.numRings = RingMenu_globalConfig.numRings - 1
+    RingMenu_UpdateAllRings()
+end
+
+function RingMenu_UpdateRing(ringID)
+    -- Lazy-init of the ringFrame array
+    RingMenu.ringFrame = RingMenu.ringFrame or {}
+    
+    local config = RingMenu_ringConfig[ringID] -- required for further setup
+    
+    if not RingMenu.ringFrame[ringID] then
+        -- Lazy-init of the ringFrame itself
+        RingMenu.ringFrame[ringID] = CreateFrame("Frame", "RingMenuRingFrame" .. ringID, UIParent)
+        local rf = RingMenu.ringFrame[ringID]
+        rf.ringID = ringID
+        
+        -- Backdrop texture
+        rf.backdrop = rf:CreateTexture(rf:GetName() .. "Backdrop", "BACKGROUND")
+        rf.backdrop:SetPoint("BOTTOMLEFT", rf, "BOTTOMLEFT")
+        rf.backdrop:SetPoint("TOPRIGHT", rf, "TOPRIGHT")
+        rf.backdrop:SetTexture("Interface\\AddOns\\RingMenu\\RingMenuBackdrop.tga")
+        
+        -- An invisible button used as a secure handler for
+        -- (a) responding to CLICK RingMenuToggleRing*:LeftButton binding events on a secure path
+        -- (b) running secure event responses for the ring button OnClick event
+        rf.toggleButton = CreateFrame("Button", "RingMenuToggleRing" .. ringID, rf, "SecureHandlerMouseUpDownTemplate")
+        rf.toggleButton:SetAttribute("downbutton", "")
+        rf.toggleButton:SetFrameRef("UIParent", UIParent)
+        rf.toggleButton:SetAttribute("_onmousedown", [[ -- (self, button)
+            local rf = self:GetParent()
+            local numRings = self:GetAttribute("numRings")
+            local allowMultipleOpenRings = self:GetAttribute("allowMultipleOpenRings")
+            local UIParent = self:GetFrameRef("UIParent")
+            
+            if rf:IsShown() then
+                rf:Hide()
+            else
+                 if not allowMultipleOpenRings then
+                    for ringID = 1, numRings do
+                        local rfOther = self:GetFrameRef("ringFrame" .. ringID)
+                        if rfOther then
+                            rfOther:Hide()
+                        end
+                    end
+                end
+                local relx, rely = UIParent:GetMousePosition()
+                local x = relx * UIParent:GetWidth()
+                local y = rely * UIParent:GetHeight()
+                rf:SetPoint("CENTER", UIParent, "BOTTOMLEFT", x, y)
+                rf:Show()
+            end
+        ]])
+        
+        rf:Hide()
+    end
+    local rf = RingMenu.ringFrame[ringID]
+    
+    local frameSize = 2 * config.radius * config.backdropScale
+    rf:SetSize(frameSize, frameSize)
+    rf.backdrop:SetVertexColor(config.backdropColor.r, config.backdropColor.g, config.backdropColor.b, config.backdropColor.a)
+    rf.toggleButton:SetAttribute("allowMultipleOpenRings", RingMenu_globalConfig.allowMultipleOpenRings)
+    rf:SetAttribute("closeOnClick", config.closeOnClick)
+    
+    -- Lazy-init this ringFrame's buttons
+    rf.button = rf.button or {}
+    for buttonID = 1, (config.numSlots or 1) do
+        if not rf.button[buttonID] then
+            rf.button[buttonID] = CreateFrame("CheckButton", "RingMenuRingFrame" .. ringID .. "Button" .. buttonID, rf, "ActionBarButtonTemplate")
+            if Masque then
+                local masqueRing = Masque:Group("RingMenu")
+                masqueRing:AddButton(rf.button[buttonID])
+            end
+            local button = rf.button[buttonID]
+            button.ringID = ringID
+            button.buttonID = buttonID
+            
+            rf.toggleButton:WrapScript(button, "OnClick", [[ -- (self, button, down)
+                local rf = self:GetParent()
+                local closeOnClick = rf:GetAttribute("closeOnClick")
+                if closeOnClick then
+                    rf:Hide()
+                end
+            ]])
+        end
+        local button = rf.button[buttonID]
+        
+        local angle = 2 * math.pi * (0.25 - (buttonID - 1) / config.numSlots - config.angle / 360.0)
+        local posX = config.radius * math.cos(angle)
+        local posY = config.radius * math.sin(angle)
+        button:SetPoint("CENTER", rf, "CENTER", posX, posY)
+        button:SetAttribute("type", "action")
+        local firstSlot = config.firstSlot or 1
+        local buttonSlot = firstSlot + buttonID - 1
+        button:SetAttribute("action", buttonSlot)
+    end
+    -- Hide unused buttons
+    for id, button in ipairs(rf.button) do
+        if id > config.numSlots then
+            button:Hide()
+        end
+    end
+end
+
+function RingMenu_UpdateRingCrossReferences()
+    for ringID = 1, RingMenu_globalConfig.numRings do
+        local rf = RingMenu.ringFrame[ringID]
+        for ringIDOther = 1, RingMenu_globalConfig.numRings do
+            local rfOther = RingMenu.ringFrame[ringIDOther]
+            if rfOther then
+                rf.toggleButton:SetFrameRef("RingFrame" .. ringIDOther, rfOther)
+            end
+        end
+        rf.toggleButton:SetAttribute("numRings", RingMenu_globalConfig.numRings)
+    end
+end
+
+function RingMenu_UpdateAllRings()
+    for ringID = 1, RingMenu_globalConfig.numRings do
+        RingMenu_UpdateRing(ringID)
+    end
+    RingMenu_UpdateRingCrossReferences()
+end
+
+-- The main frame is used only to respond to global events
+RingMenu.mainFrame = CreateFrame("Frame")
+RingMenu.mainFrame.OnEvent = function (self, event, arg1)
+    if event == "ADDON_LOADED" and arg1 == RingMenu_AddonName then
+        -- Update empty fields in settings with default values
+        RingMenu_globalConfig = RingMenu_globalConfig or {}
+        RingMenu.update_with_defaults(RingMenu_globalConfig, RingMenu_globalConfigDefault)
+        for ringID = 1, RingMenu_globalConfig.numRings do
+            RingMenu_ringConfig[ringID] = RingMenu_ringConfig[ringID] or {}
+            RingMenu.update_with_defaults(RingMenu_ringConfig[ringID], RingMenu_ringConfigDefault)
+        end
+        
+        -- Init state
+        RingMenu.globalState = RingMenu.deep_copy(RingMenu_globalStateDefault)
+        for ringID = 1, RingMenu_globalConfig.numRings do
+            RingMenu.ringState[ringID] = RingMenu.deep_copy(RingMenu_ringStateDefault)
+        end
+        
+        RingMenu_UpdateAllRings()
+        
+        -- Init options panel
+        RingMenuOptions_SetupPanel()
+    end
+end
+RingMenu.mainFrame:RegisterEvent("ADDON_LOADED")
+RingMenu.mainFrame:SetScript("OnEvent", RingMenu.mainFrame.OnEvent)
+
+SLASH_RINGMENU1 = '/ringmenu'
+function SlashCmdList.RINGMENU(msg, editBox)
+    -- Workaround: this function has to be called twice
+    Settings.OpenToCategory("RingMenu")
+end
---
+++ options.lua
@@ -0,0 +1,422 @@
+local RingMenu_AddonName, RingMenu = ...
+
+local function getRingBindingCommand(ringID)
+    local ringFrame = RingMenu.ringFrame[ringID]
+    local toggleButton = ringFrame.toggleButton
+    return "CLICK " .. toggleButton:GetName() .. ":LeftButton"
+end
+
+local function getRingBindingKeyBinds(ringID)
+    local command = getRingBindingCommand(ringID)
+    return { GetBindingKey(command) }
+end
+
+local function unbindAllRingBindingKeyBinds(ringID)
+    local keyBinds = getRingBindingKeyBinds(ringID)
+    for _, keyBind in ipairs(keyBinds) do
+        SetBinding(keyBind)
+    end
+end
+
+local function getRingBindingKeyBindsText(ringID)
+    local keyBinds = getRingBindingKeyBinds(ringID)
+    if #keyBinds > 0 then
+        return GetBindingText(keyBinds[1])
+    else
+        return "(not bound)"
+    end
+end
+
+local function getRingDropdownText(ringID)
+    local ringName = RingMenu_ringConfig[ringID].name
+    local bindingText = getRingBindingKeyBindsText(ringID)
+    if ringName and strlen(ringName) > 0 then
+        return bindingText .. ": " .. ringName
+    else
+        return bindingText
+    end
+end
+
+local function restoreAllSavedKeyBinds()
+    for ringID = 1, RingMenu_globalConfig.numRings do
+        local ringConfig = RingMenu_ringConfig[ringID]
+        if ringConfig.keyBind then
+            SetBinding(ringConfig.keyBind, getRingBindingCommand(ringID))
+        end
+    end
+    AttemptToSaveBindings(GetCurrentBindingSet())
+end
+
+function RingMenuOptionsPanel_AddRing()
+    PlaySound(624) -- GAMEGENERICBUTTONPRESS
+    local ringPanel = _G["RingMenuOptionsPanelRingConfig"]
+    local ringID = RingMenu_AddRing()
+    RingMenuOptionsPanel.currentRingID = ringID
+    RingMenu_UpdateAllRings()
+    ringPanel.refresh()
+end
+
+function RingMenuOptionsPanel_RemoveRing()
+    PlaySound(624) -- GAMEGENERICBUTTONPRESS
+    if RingMenu_globalConfig.numRings <= 1 then
+        PlaySound(847) -- igQuestFailed
+        return
+    end
+
+    local ringPanel = _G["RingMenuOptionsPanelRingConfig"]
+    
+    unbindAllRingBindingKeyBinds(RingMenuOptionsPanel.currentRingID)
+    RingMenu_RemoveRing(RingMenuOptionsPanel.currentRingID)
+    if RingMenuOptionsPanel.currentRingID > RingMenu_globalConfig.numRings then
+        RingMenuOptionsPanel.currentRingID = RingMenu_globalConfig.numRings
+    end
+    
+    restoreAllSavedKeyBinds()
+    RingMenu_UpdateAllRings()
+    ringPanel.refresh()
+end
+
+RingMenu.ringConfigWidgets = {
+    {
+        name = "keyBind",
+        label = "Key Binding",
+        widgetType = "keyBind",
+    },
+    {
+        name = "name",
+        label = "Name",
+        widgetType = "text",
+    },
+    {
+        name = "firstSlot",
+        label = "First Button Slot",
+        widgetType = "number",
+        tooltip = "The action button slot that is used for the first button in the RingMenu.",
+    },
+    {
+        name = "numSlots",
+        label = "Number of Buttons",
+        widgetType = "slider",
+        min = 1, max = 24, labelSuffix = "", valueStep = 1,
+    },
+    {
+        name = "closeOnClick",
+        label = "Close on Click",
+        widgetType = "checkbox",
+    },
+    {
+        name = "backdropColor",
+        label = "Backdrop Color",
+        widgetType = "color",
+    },
+    {
+        name = "radius",
+        label = "Radius",
+        widgetType = "slider",
+        min = 0, max = 300, labelSuffix = " px", valueStep = 1,
+    },
+    {
+        name = "angle",
+        label = "Angle",
+        widgetType = "slider",
+        min = 0, max = 360, labelSuffix = " °", valueStep = 1,
+    },
+}
+
+function RingMenuOptions_SetupPanel()
+    local panel = _G["RingMenuOptionsPanel"]
+    local ringPanel = _G["RingMenuOptionsPanelRingConfig"]
+    local ringDropdown = _G["RingMenuOptionsPanelRingDropDown"]
+    
+    -- Setup the drop down menu
+    
+    panel.currentRingID = 1
+    
+    function ringDropdown.Clicked(self, ringID, arg2, checked)
+        RingMenuOptionsPanel.currentRingID = ringID
+        ringPanel.refresh()
+    end
+    
+    function ringDropdown.Menu()
+        for ringID = 1, RingMenu_globalConfig.numRings do
+            local info = UIDropDownMenu_CreateInfo()
+            info.text = getRingDropdownText(ringID)
+            info.value = ringID
+            info.checked = (ringID == RingMenuOptionsPanel.currentRingID)
+            info.func = ringDropdown.Clicked
+            info.arg1 = ringID
+            UIDropDownMenu_AddButton(info)
+        end
+    end
+    UIDropDownMenu_Initialize(ringDropdown, ringDropdown.Menu)
+    UIDropDownMenu_SetWidth(ringDropdown, 200)
+    UIDropDownMenu_JustifyText(ringDropdown, "LEFT")
+    UIDropDownMenu_SetText(ringDropdown, getRingDropdownText(RingMenuOptionsPanel.currentRingID))
+    
+    -- Setup the per-ring configuration panel
+    
+    local function appendWidget(parent, child, rowPadding)
+        child:SetParent(parent)
+        if parent.lastWidget then
+            child:SetPoint("TOPLEFT", parent.lastWidget, "BOTTOMLEFT", 0, -rowPadding)
+        else
+            child:SetPoint("TOPLEFT", parent, "TOPLEFT", 16, -16)
+        end
+        parent.lastWidget = child
+    end
+    
+    local labelWidth = 160
+    local widgetWidth = 180
+    local columnPadding = 0
+    local rowPadding = 24
+    
+    local function refreshWidget(widget)
+        local widgetFrame = widget.widgetFrame
+        if widgetFrame then
+            local settingsTable = RingMenu_ringConfig[RingMenuOptionsPanel.currentRingID]
+            local settingsField = widget.name
+            local value = settingsTable[settingsField]
+            if widget.widgetType == "slider" then
+                widgetFrame:SetValue(value)
+            elseif widget.widgetType == "checkbox" then
+                widgetFrame:SetChecked(value)
+            elseif widget.widgetType == "text" then
+                widgetFrame:SetText(value or "")
+                widgetFrame:SetCursorPosition(0) -- Fix to scroll the text field to the left
+                widgetFrame:ClearFocus()
+            elseif widget.widgetType == "number" then
+                widgetFrame:SetText(value or 0)
+                widgetFrame:SetCursorPosition(0) -- Fix to scroll the text field to the left
+                widgetFrame:ClearFocus()
+            elseif widget.widgetType == "keyBind" then
+                widgetFrame:SetText(getRingBindingKeyBindsText(RingMenuOptionsPanel.currentRingID))
+            elseif widget.widgetType == "color" then
+                local texture = widgetFrame.texture
+                texture:SetVertexColor(value.r, value.g, value.b, value.a)
+            else
+                print("Unexpected widget type " .. widget.widgetType)
+            end
+        end
+    end
+    
+    -- This is the method that actually updates the settings field in the RingMenu_ringConfig table
+    local function widgetChanged(widget, value)
+        local settingsTable = RingMenu_ringConfig[RingMenuOptionsPanel.currentRingID]
+        local settingsField = widget.name
+        settingsTable[settingsField] = value
+        RingMenu_UpdateRing(RingMenuOptionsPanel.currentRingID)
+        
+        -- Some config panel changes that should take immediate effect
+        UIDropDownMenu_SetText(ringDropdown, getRingDropdownText(RingMenuOptionsPanel.currentRingID))
+    end
+    
+    local function sliderOnValueChanged(self, value, isUserInput)
+        local widget = self.widget
+        local label = _G[self:GetName() .. "Text"]
+        local suffix = widget.labelSuffix or ""
+        label:SetText(value .. suffix)
+        
+        if isUserInput then
+            widgetChanged(widget, value)
+        end
+    end
+    
+    local function checkboxOnClick(self)
+        local widget = self.widget
+        local value = (not not self:GetChecked())
+        widgetChanged(widget, value)
+    end
+    
+    local function textOnValueChanged(self, isUserInput)
+        if not isUserInput then
+            return
+        end
+        local widget = self.widget
+        local value = self:GetText()
+        widgetChanged(widget, value)
+    end
+    
+    local function numberOnValueChanged(self, isUserInput)
+        if not isUserInput then
+            return
+        end
+        local widget = self.widget
+        local value = tonumber(self:GetText())
+        widgetChanged(widget, value)
+    end
+    
+    local keyBindHandler = CustomBindingHandler:CreateHandler("RingMenuToggle")
+    
+    local function keyBindOnBindingCompleted(self, completedSuccessfully, keys)
+        if completedSuccessfully then
+            if keys then
+                unbindAllRingBindingKeyBinds(RingMenuOptionsPanel.currentRingID)
+            
+                -- Workaround: Sanitize modifier key names
+                local metaKeyMap = {
+                    ['LALT'] = 'ALT',
+                    ['RALT'] = 'ALT',
+                    ['LCTRL'] = 'CTRL',
+                    ['RCTRL'] = 'CTRL',
+                    ['LSHIFT'] = 'SHIFT',
+                    ['RSHIFT'] = 'SHIFT',
+                }
+                for i, k in pairs(keys) do
+                    keys[i] = metaKeyMap[k] or k
+                end
+                
+                local keyBind = CreateKeyChordStringFromTable(keys)
+                local command = getRingBindingCommand(RingMenuOptionsPanel.currentRingID)
+                SetBinding(keyBind, command)
+                AttemptToSaveBindings(GetCurrentBindingSet())
+                
+                widgetChanged(self.widget, keyBind)
+            end
+        end
+        self:SetText(getRingBindingKeyBindsText(RingMenuOptionsPanel.currentRingID))
+    end
+    
+    local function colorOnClick(self)
+        local widget = self.widget
+        local settingsTable = RingMenu_ringConfig[RingMenuOptionsPanel.currentRingID]
+        local settingsField = widget.name
+        local color = settingsTable[settingsField]
+        
+        ColorPickerFrame:SetColorRGB(color.r, color.g, color.b)
+        ColorPickerFrame.hasOpacity = true
+        ColorPickerFrame.opacity = color.a
+        ColorPickerFrame.previousValues = {color.r, color.g, color.b, color.a}
+        local colorPickerCallback = function (restore)
+            local value = {}
+            if restore then
+                value.r, value.g, value.b, value.a = unpack(restore)
+            else
+                value.r, value.g, value.b = ColorPickerFrame:GetColorRGB()
+                value.a = OpacitySliderFrame:GetValue()
+            end
+            -- Color preview
+            widget.widgetFrame.texture:SetVertexColor(value.r, value.g, value.b, value.a)
+            widgetChanged(self.widget, value)
+        end
+        ColorPickerFrame.func = colorPickerCallback
+        ColorPickerFrame.opacityFunc = colorPickerCallback
+        ColorPickerFrame.cancelFunc = colorPickerCallback
+        ColorPickerFrame:Hide()
+        ColorPickerFrame:Show()
+    end
+    
+    for _, widget in ipairs(RingMenu.ringConfigWidgets) do
+        local label = ringPanel:CreateFontString(ringPanel:GetName() .. "Label" .. widget.name, "ARTWORK", "GameFontNormal")
+        label:SetText(widget.label)
+        label:SetWidth(labelWidth)
+        label:SetJustifyH("LEFT")
+        appendWidget(ringPanel, label, rowPadding)
+    
+        local widgetFrame = nil
+        
+        if widget.widgetType == "slider" then
+            widgetFrame = CreateFrame("Slider", ringPanel:GetName() .. "Widget" .. widget.name, ringPanel, "OptionsSliderTemplate")
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding, 0)
+            widgetFrame:SetWidth(widgetWidth)
+            widgetFrame:SetHeight(17)
+            widgetFrame:SetMinMaxValues(widget.min, widget.max)
+            if widget.valueStep then
+                widgetFrame:SetValueStep(widget.valueStep)
+                widgetFrame:SetObeyStepOnDrag(true)
+            end
+            widgetFrame:SetValue(widget.min)
+            local lowLabel = widget.min
+            local highLabel = widget.max
+            if widget.labelSuffix then
+                lowLabel = lowLabel .. widget.labelSuffix
+                highLabel = highLabel .. widget.labelSuffix
+            end
+            _G[widgetFrame:GetName().."Low"]:SetText(lowLabel)
+            _G[widgetFrame:GetName().."High"]:SetText(highLabel)
+
+            widgetFrame:SetScript("OnValueChanged", sliderOnValueChanged)
+        elseif widget.widgetType == "checkbox" then
+            widgetFrame = CreateFrame("CheckButton", ringPanel:GetName() .. "Widget" .. widget.name, ringPanel, "UICheckButtonTemplate")
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding - 2, 0)
+            
+            widgetFrame:SetScript("OnClick", checkboxOnClick)
+        elseif widget.widgetType == "text" then
+            widgetFrame = CreateFrame("EditBox", ringPanel:GetName() .. "Widget" .. widget.name, ringPanel, "InputBoxTemplate")
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding + 6, 0)
+            widgetFrame:SetWidth(widgetWidth - 6)
+            widgetFrame:SetHeight(20)
+            widgetFrame:SetAutoFocus(false)
+            
+            widgetFrame:SetScript("OnTextChanged", textOnValueChanged)
+        elseif widget.widgetType == "number" then
+            widgetFrame = CreateFrame("EditBox", ringPanel:GetName() .. "Widget" .. widget.name, ringPanel, "InputBoxTemplate")
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding + 6, 0)
+            widgetFrame:SetWidth(40)
+            widgetFrame:SetHeight(20)
+            widgetFrame:SetAutoFocus(false)
+            widgetFrame:SetNumeric(true)
+            widgetFrame:SetMaxLetters(3)
+            
+            widgetFrame:SetScript("OnTextChanged", numberOnValueChanged)
+        elseif widget.widgetType == "keyBind" then
+            widgetFrame = CustomBindingManager:RegisterHandlerAndCreateButton(keyBindHandler, "CustomBindingButtonTemplateWithLabel", ringPanel)
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding - 1, 0)
+            widgetFrame:SetWidth(widgetWidth + 2)
+            
+            keyBindHandler:SetOnBindingCompletedCallback(function (completedSuccessfully, keys)
+                keyBindOnBindingCompleted(widgetFrame, completedSuccessfully, keys)
+            end)
+        elseif widget.widgetType == "color" then
+            widgetFrame = CreateFrame("Button", ringPanel:GetName() .. "Widget" .. widget.name, ringPanel)
+            widgetFrame:SetPoint("LEFT", label, "RIGHT", columnPadding + 2, 0)
+            widgetFrame:SetSize(18, 18)
+            
+            local texture = widgetFrame:CreateTexture(nil, "BACKGROUND")
+            texture:SetPoint("CENTER", widgetFrame, "CENTER")
+            texture:SetSize(18, 18)
+            texture:SetColorTexture(0.8, 0.8, 0.8, 1.0)
+            
+            widgetFrame:SetNormalTexture("Interface/ChatFrame/ChatFrameColorSwatch")
+            local normalTexture = widgetFrame:GetNormalTexture()
+            normalTexture:SetPoint("TOPLEFT", widgetFrame, "TOPLEFT")
+            normalTexture:SetPoint("BOTTOMRIGHT", widgetFrame, "BOTTOMRIGHT")
+            
+            widgetFrame.texture = normalTexture
+            widgetFrame:SetScript("OnClick", colorOnClick)
+        else
+            print("RingMenu: Unrecognized widget type: " .. widget.widgetType)
+        end
+        if widgetFrame then
+            if widget.tooltip then
+                widgetFrame.tooltipText = widget.tooltip
+            end
+            -- Establish cross-references
+            widgetFrame.widget = widget
+            widget.widgetFrame = widgetFrame
+        end
+    end
+    
+    function ringPanel.refresh()
+        UIDropDownMenu_SetText(ringDropdown, getRingDropdownText(RingMenuOptionsPanel.currentRingID))
+        for _, widget in ipairs(RingMenu.ringConfigWidgets) do
+            refreshWidget(widget)
+        end
+    end
+    
+    -- Display the current version in the title
+    local version = GetAddOnMetadata("RingMenu", "Version")
+    local titleLabel = _G["RingMenuOptionsPanelTitle"]
+    titleLabel:SetText("RingMenu |cFF888888v" .. version)
+    
+    panel.name = "RingMenu"
+    panel.refresh = function (self)
+        ringPanel.refresh()
+    end
+    -- panel.okay
+    -- panel.cancel
+    -- panel.default
+    category, layout = Settings.RegisterCanvasLayoutCategory(panel, panel.name, panel.name);
+    category.ID = panel.name
+    Settings.RegisterAddOnCategory(category);
+end
+  
